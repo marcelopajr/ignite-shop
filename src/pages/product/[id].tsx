d6030817e875b19b513c5510ad714b188d4693db
marcelopajr <marcelo96jr@gmail.com>
2023-06-28 19:25:52 -0300

    chore: add product page structure and remove static photos

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,27 @@
-import { useRouter } from "next/router";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "../../styles/pages/product";
 
 export default function Product() {
-  const { query } = useRouter();
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
 
-  return <h1>Product: {JSON.stringify(query)}</h1>;
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio libero
+          sit voluptatum. Distinctio molestiae at odio, sapiente sequi
+          molestias? Sapiente, quod. Quisquam alias harum quibusdam? Laboriosam,
+          dicta sint. Placeat, et!
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
